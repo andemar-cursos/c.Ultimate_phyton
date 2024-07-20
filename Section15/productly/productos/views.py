05f037c85f9f5a7428294f45aac1d7125eb40919
Anderson Martinez <andemartinezdev@gmail.com>
2024-07-19 22:16:50 -0500

    Section 15 - 148. db

--- Section15/productly/productos/views.py
+++ Section15/productly/productos/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from productos.models import Producto
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola mundo!')
+    productos = Producto.objects.all().values()
+    return JsonResponse(list(productos), safe=False)
 
