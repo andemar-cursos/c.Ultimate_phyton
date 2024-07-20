cdedd343cf46abd7fd5377c9cafb8e51327d6792
Anderson Martinez <andemartinezdev@gmail.com>
2024-07-19 22:26:21 -0500

    Section 15 - 150. Plantillas

--- Section15/productly/productos/views.py
+++ Section15/productly/productos/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 
 from productos.models import Producto
 
@@ -6,5 +7,10 @@ from productos.models import Producto
 # Create your views here.
 def index(request):
     productos = Producto.objects.all().values()
-    return JsonResponse(list(productos), safe=False)
+
+    return render(
+        request,
+        'index.html',
+        context={'productos': productos}
+    )
 
